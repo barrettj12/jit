9dd3f360a67a9460b5ab5c45dc3c8b5cb0522c32
Jordan Barrett <jordan.barrett@canonical.com>
2022-10-07 17:05:12 +1100

    use raw git for clone

--- cmd/clone.go
+++ cmd/clone.go
@@ -3,48 +3,96 @@ package cmd
 import (
 	"fmt"
 	"github.com/barrettj12/jit/common"
-	"github.com/go-git/go-git/v5"
 	"net/url"
 	"path/filepath"
+	"strings"
 )
 
 // Clone clones the provided repo, using the workflow described in
 // https://morgan.cugerone.com/blog/how-to-use-git-worktree-and-in-a-clean-way/
 // TODO: ensure fork is created
 func Clone(args []string) error {
-	if len(args) < 1 {
-		return fmt.Errorf("please provide URL to clone")
+	var user, repo string
+
+	var arg1 string
+	err := common.ReqArg(args, 0, "URL of Git repo to clone:", &arg1)
+	if err != nil {
+		return err
 	}
-	urlStr := args[0]
+	user, repo, err = parseGitRepoURL(arg1)
+	if err != nil {
+		return err
+	}
+
+	if repo == "" {
+		// arg1 was just "user"
+		err := common.ReqArg(args, 1, fmt.Sprint("Which of %s's repos do you want?", user), &repo)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Reconstruct repository URL
+	repoURL := fmt.Sprintf("https://github.com/%s/%s", user, repo)
 
 	// Use JIT_DIR to find clone path
 	jitDir, err := common.JitDir()
 	if err != nil {
 		return err
 	}
-
-	u, err := url.Parse(urlStr)
-	if err != nil {
-		return fmt.Errorf("error parsing url: %w", err)
-	}
-	cloneDir := filepath.Join(jitDir, u.Path)
+	cloneDir := filepath.Join(jitDir, user, repo)
 
 	// Make a bare repo
-	_, err = git.PlainClone(filepath.Join(cloneDir, ".git"), true, &git.CloneOptions{
-		URL:        urlStr,
-		RemoteName: "source",
-	})
+	// git clone --bare <repository> <directory>
+	err = common.Git("clone", []string{"--bare", repoURL, cloneDir})
 	if err != nil {
 		return fmt.Errorf("error cloning repo: %w", err)
 	}
 
+	// Add remote "fork"
+
 	// Success - print message to user
 	fmt.Printf(`
-Successfully cloned repo %v into %v
+Successfully cloned repo %s/%s into %v
 Copy remote branches using
     jit get <remote-branch>
 or create new branches using
     jit new <branch> <base>
-`[1:], urlStr, cloneDir)
+`[1:], user, repo, cloneDir)
 	return nil
+}
+
+// Parses the given argument to a GitHub user & repo.
+// Valid inputs are
+//
+//	"user"                         -> "user", "",     nil
+//	"user/repo"                    -> "user", "repo", nil
+//	"https://github.com/user/repo" -> "user", "repo", nil
+func parseGitRepoURL(raw string) (string, string, error) {
+	u, err := url.Parse(raw) // only matches full URL with scheme
+	if err != nil {
+		return "", "", err
+	}
+
+	switch u.Host {
+	case "":
+	// raw is not a URL
+	case "github.com":
+		raw = u.Path[1:] // strip leading '/'
+	default:
+		return "", "", fmt.Errorf("host %s not supported", u.Host)
+	}
+
+	// raw is now "user" or "user/repo"
+	parts := strings.Split(raw, "/")
+	switch len(parts) {
+	case 1:
+		// "user"
+		return parts[0], "", nil
+	case 2:
+		// "user/repo"
+		return parts[0], parts[1], nil
+	default:
+		return "", "", fmt.Errorf("invalid Git repo URL %s", raw)
+	}
 }
