42dc2bcaa7efc165f4b88c633ca19b5e0e8ffbe1
Jordan Barrett <jordan.barrett@canonical.com>
2022-11-17 15:31:16 +1100

    add ExecGit function

--- common/common.go
+++ common/common.go
@@ -2,12 +2,10 @@ package common
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"strings"
 )
 
@@ -51,32 +49,14 @@ func ReqArg(args []string, i int, prompt string) (string, error) {
 	return sc.Text(), nil
 }
 
-var GitNotARepoErr = regexp.MustCompile("not a git repository")
-
 // Returns the root folder of the current repo
 func RepoBasePath() (string, error) {
-	// TODO: factor out this exec.Command logic
-	stdout := bytes.Buffer{}
-	stderr := bytes.Buffer{}
-
-	cmd := exec.Command("git", "rev-parse", "--path-format=absolute", "--git-common-dir")
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
+	stdout, err := ExecGit("rev-parse", "--path-format=absolute", "--git-common-dir")
 	if err != nil {
-		// Read stderr for error info
-		errInfo := stderr.String()
-
-		if GitNotARepoErr.MatchString(errInfo) {
-			return "", fmt.Errorf("current dir is not inside a git repo")
-		} else {
-			return "", fmt.Errorf("%s\n%s", errInfo, err)
-		}
+		return "", err
 	}
 
-	// Success - return dir of stdout
-	path := filepath.Dir(stdout.String())
+	path := filepath.Dir(stdout)
 	return path, nil
 }
 
@@ -94,27 +74,13 @@ var ErrUpstreamNotFound = fmt.Errorf("upstream not found")
 
 // Returns push location (remote, branch) for the given branch
 func PushLoc(localBranch string) (remote, remoteBranch string, err error) {
-	stdout := bytes.Buffer{}
-	stderr := bytes.Buffer{}
-
-	cmd := exec.Command("git", "for-each-ref", "--format='%(push:short)'",
+	stdout, err := ExecGit("for-each-ref", "--format='%(push:short)'",
 		fmt.Sprintf("refs/heads/%s", localBranch))
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err = cmd.Run()
 	if err != nil {
-		// Read stderr for error info
-		errInfo := stderr.String()
-
-		if GitNotARepoErr.MatchString(errInfo) {
-			return "", "", fmt.Errorf("current dir is not inside a git repo")
-		} else {
-			return "", "", fmt.Errorf("%s\n%s", errInfo, err)
-		}
+		return "", "", err
 	}
 
-	pushloc := strings.Trim(stdout.String(), "'\n")
+	pushloc := strings.Trim(stdout, "'\n")
 	if pushloc == "" {
 		return "", "", ErrUpstreamNotFound
 	}
---
+++ common/git.go
@@ -0,0 +1,29 @@
+package common
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var GitNotARepoErr = regexp.MustCompile("not a git repository")
+
+func ExecGit(args ...string) (string, error) {
+	res := Exec(ExecArgs{
+		Cmd:  "git",
+		Args: args,
+	})
+
+	// handle errors
+	if res.RunError != nil {
+		// Read stderr for error info
+		errInfo := res.Stderr
+
+		if GitNotARepoErr.MatchString(errInfo) {
+			return "", fmt.Errorf("current dir is not inside a git repo")
+		} else {
+			return "", fmt.Errorf("%s\n%s", errInfo, res.RunError)
+		}
+	}
+
+	return res.Stdout, nil
+}
