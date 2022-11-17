be8e7f20b0d04e1b7ec85aa6cf357a3ff79179d6
Jordan Barrett <jordan.barrett@canonical.com>
2022-11-17 15:30:50 +1100

    add Exec function

---
+++ common/exec.go
@@ -0,0 +1,55 @@
+package common
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
+)
+
+// Nice interface for running command line methods
+type ExecArgs struct {
+	Cmd  string
+	Args []string
+	Dir  string
+
+	Stdout, Stderr io.Writer
+}
+
+type ExecResult struct {
+	RunError error
+	// output
+	Stdout, Stderr, Combined string
+}
+
+func Exec(args ExecArgs) ExecResult {
+	cmd := exec.Command(args.Cmd, args.Args...)
+	cmd.Dir = args.Dir
+
+	// Set up stdin/out/err
+	cmd.Stdin = os.Stdin
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	combined := &bytes.Buffer{}
+
+	outWriters := []io.Writer{stdout, combined}
+	if args.Stdout != nil {
+		outWriters = append(outWriters, args.Stdout)
+	}
+	cmd.Stdout = io.MultiWriter(outWriters...)
+
+	errWriters := []io.Writer{stderr, combined}
+	if args.Stderr != nil {
+		errWriters = append(errWriters, args.Stderr)
+	}
+	cmd.Stderr = io.MultiWriter(errWriters...)
+
+	runErr := cmd.Run() // this error contains the exit code
+	return ExecResult{
+		RunError: runErr,
+		Stdout:   stdout.String(),
+		Stderr:   stderr.String(),
+		Combined: combined.String(),
+	}
+}
